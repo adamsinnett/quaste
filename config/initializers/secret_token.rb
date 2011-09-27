7a2c3221d9915a706cef4d8f814f74b05f5265f4
Adam Sinnett <quandrum@gmail.com>
2011-09-27 10:33:00 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quaste::Application.config.secret_token = '0524de29c8de7bdffe91eb18991421daa2b56c5a4a1891d8c47df8c611a655e4abb7a0e7b0c641dfab0b1a8165fc98211d1a80bd198a428c1e22a47cfeebe80c'
